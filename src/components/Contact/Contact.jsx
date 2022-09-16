fde935d07243f6b2a99a5684f32e79c056ed3c5d
Kris <kris@syevens.com>
2022-09-16 02:44:25 -0500

    Update Commit - Contact Component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,90 @@
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_job9r4r",
+        "template_h5049op",
+        form.current,
+        "9LFb-QebuCoVqzYVs"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>kris@syevens.com</h5>
+            <a href="mailto:kris@syevens.com" target="_blank" rel="noreferrer">
+              Send An Email
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaTwitter className="contact__option-icon" />
+            <h4>Direct Message</h4>
+            <h5>@kris_syevens</h5>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              DM Via Twitter
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone+123456789"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send A Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 export default Contact;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,7 +10,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {portfolioData.map((item) => {
-          return <PortfolioItem id={item.id} {...item} />;
+          return <PortfolioItem key={item.id} {...item} />;
         })}
       </div>
     </section>
