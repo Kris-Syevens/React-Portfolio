1fcf1cf447972385162a2cd47397ad0c81daaa95
Kris <kris@syevens.com>
2022-09-17 03:16:36 -0500

    Update Commit - Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,47 @@
 import "./footer.css";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#header" className="footer__logo">
+        Kris-Syevens
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#tech">Tech</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://linkedin.com">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com">
+          <FaGithub />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2023 Kris-Syevens All rights reserved. </small>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
