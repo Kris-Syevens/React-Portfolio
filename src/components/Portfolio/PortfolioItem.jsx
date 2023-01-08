8e43f4c47340ac097552dc8792861998ecabac57
Kris <kris@syevens.com>
2023-01-08 14:52:58 -0600

    Update Commit - Corrected Portfolio Section

--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -1,11 +1,13 @@
 const PortfolioItem = ({ image, title, github, demo, description }) => {
   return (
     <article className="portfolio__item">
-      <div className="portfolio__item-image">
-        <img src={image} alt={title} />
+      <div className="portfolio__content__wrapper">
+        <div className="portfolio__item-image">
+          <img src={image} alt={title} />
+        </div>
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
-      <h3>{title}</h3>
-      <p>{description}</p>
       <div className="portfolio__item-cta">
         <a href={github} target="_blank" rel="noreferrer" className="btn">
           Github
