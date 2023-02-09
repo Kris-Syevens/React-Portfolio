239399faa9cdd2701113bb864d76bbe796a11bc5
Kris <kris@syevens.com>
2023-02-09 00:05:22 -0600

    Update Commit - Img path correction

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import IMG1 from "./portfolio1.png";
-import IMG2 from "./portfolio2.jpg";
-import IMG3 from "./portfolio3.jpg";
+import IMG2 from "./portfolio2.png";
+import IMG3 from "./portfolio3.png";
 
 export const Images = {
   IMG1,
